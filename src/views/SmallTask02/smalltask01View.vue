fee9da97227e8f30a5971c57e2cc7e7825b9206c
vow717 <147890905+vow717@users.noreply.github.com>
2024-07-03 21:48:38 +0800

    update

--- src/views/SmallTask02/smalltask01View.vue
+++ src/views/SmallTask02/smalltask01View.vue
@@ -1,15 +1,24 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import ChildComponent from './smalltask02View.vue'
+
+const componentsToShow = ref(false)
+
+const addComponent = () => {
+  componentsToShow.value = true
+}
 </script>
 <template>
   <div>
-    <h4>第一次加载，可能慢一些</h4>
-    <ChildComponent />
+    <button @click="addComponent()">加载组件</button>
     <hr />
-    <h4>已经加载过</h4>
-    <ChildComponent />
+    <p>第一次，可能慢点</p>
+    <ChildComponent v-if="componentsToShow" />
     <hr />
-    <h4>已经加载过</h4>
-    <ChildComponent />
+    <p>已经加载过了</p>
+    <ChildComponent v-if="componentsToShow" />
+    <hr />
+    <p>已经加载过了</p>
+    <ChildComponent v-if="componentsToShow" />
   </div>
 </template>
