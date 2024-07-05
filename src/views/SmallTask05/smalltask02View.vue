2e3300fbce2229b5228632a137d142fc5e0060dc
vow717 <147890905+vow717@users.noreply.github.com>
2024-07-05 12:31:32 +0800

    update

--- src/views/SmallTask05/smalltask02View.vue
+++ src/views/SmallTask05/smalltask02View.vue
@@ -4,6 +4,7 @@ import { usesmalltask01Store } from './smalltask01Store'
 import type { Teacher } from './smalltask01Mock'
 const { fetchTeachersByDepartment } = usesmalltask01Store()
 
+const loading = ref<boolean>(false)
 const props = defineProps<{ departmentId: string }>()
 const selectedTeacher = ref<Teacher>({})
 const teachersR = ref<Teacher[]>([])
@@ -11,18 +12,35 @@ defineExpose<{ teacher: Ref<Teacher> }>({ teacher: selectedTeacher })
 watch(
   () => props.departmentId,
   async () => {
+    loading.value = true
     selectedTeacher.value = {}
     teachersR.value = props.departmentId ? await fetchTeachersByDepartment(props.departmentId) : []
+    loading.value = false
   }
 )
 </script>
 <template>
   <div v-if="departmentId">
     <h3>选择教师</h3>
-    <select v-model="selectedTeacher">
-      <option v-for="(teacher, index) of teachersR" :key="index" :value="teacher">
-        {{ teacher.name }}
-      </option>
-    </select>
+    <div v-if="loading">
+      <img
+        src="https://img.zcool.cn/community/01a25e5d6220d0a8012187f413dea0.gif"
+        alt="Loading..." />
+    </div>
+    <div v-else>
+      <select v-model="selectedTeacher">
+        <option v-for="(teacher, index) of teachersR" :key="index" :value="teacher">
+          {{ teacher.name }}
+        </option>
+      </select>
+    </div>
   </div>
 </template>
+
+<style>
+img {
+  margin: 10px;
+  height: 40px;
+  width: 60px;
+}
+</style>
