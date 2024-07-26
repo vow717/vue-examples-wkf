da94a32eb4728987bceb8455647ad32f1f9afb07
vow717 <147890905+vow717@users.noreply.github.com>
2024-07-26 14:48:30 +0800

    update

--- src/views/SmallTask08/xlsxView.vue
+++ src/views/SmallTask08/xlsxView.vue
@@ -21,27 +21,25 @@ import * as XLSX from 'xlsx'
 import { ref } from 'vue'
 const jsonData = ref([])
 
-const handleFileChange = (event) => {
+const handleFileChange = async (event) => {
   const file = event.target.files[0] // 获取用户选择的文件
-  if (file) {
+  const data = await readFileAsync(file) // 读取文件内容
+  const workbook = XLSX.read(data, { type: 'array' }) // 使用 xlsx 库以 array 解析数据
+
+  const firstSheetName = workbook.SheetNames[0] // 获取第一个工作表名称
+  const worksheet = workbook.Sheets[firstSheetName] // 获取第一个工作表内容
+
+  jsonData.value = XLSX.utils.sheet_to_json(worksheet) // 将工作表内容转换为 JSON 格式
+
+  console.log(jsonData.value) // 在控制台打印 JSON 数据
+}
+
+// 异步读取文件内容
+const readFileAsync = (file) => {
+  return new Promise((resolve) => {
     const reader = new FileReader() // 创建 FileReader 实例
     reader.readAsArrayBuffer(file) // 读取文件内容为 ArrayBuffer
-    reader.onload = (e) => {
-      // 定义 onload 事件处理程序
-      const data = e.target.result
-      const workbook = XLSX.read(data, { type: 'binary' }) // 使用 xlsx 库以二进制解析数据
-
-      const firstSheetName = workbook.SheetNames[0] // 获取第一个工作表名称
-      const worksheet = workbook.Sheets[firstSheetName] // 获取第一个工作表内容
-
-      jsonData.value = XLSX.utils.sheet_to_json(worksheet) // 将工作表内容转换为 JSON 格式
-
-      console.log(jsonData.value) // 在控制台打印 JSON 数据
-    }
-  }
+    reader.onload = () => resolve(reader.result) // 成功读取时返回数据
+  })
 }
 </script>
-
-<style scoped>
-/* 你的样式 */
-</style>
